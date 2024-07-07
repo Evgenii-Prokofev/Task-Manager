47578909b27db776f169b7ab647dc4a76c99d577
Евгений <prokofev_619@mail.ru>
2024-07-07 18:42:26 +0300

    second step of the project

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True' 
 
 ALLOWED_HOSTS = [
     'webserver',
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager',
+    'django_bootstrap5',
+    'task_manager',    
 ]
 
 MIDDLEWARE = [
