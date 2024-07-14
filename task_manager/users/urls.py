1b132d5432e8614d7e93761b46a6f8547d2129a3
Евгений <prokofev_619@mail.ru>
2024-07-14 22:53:24 +0300

    create authorisation form

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from task_manager.users.views import (
     UsersListView, UserCreateView,
+    UserUpdateView,
 )
 
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', UsersListView.as_view(), name='users'),
     path('create/', UserCreateView.as_view(),
         name='user_create'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), 
+        name='user_update'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,10 +2,11 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from task_manager.users.models import User
 from task_manager.users.forms import UserCreateForm
 from django.utils.translation import gettext_lazy as _
+from task_manager.mixins import UserLoginMixin, UserPermitModifyMixin
 
 
 # Create your views here.
@@ -22,3 +23,14 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserCreateForm
     success_url = reverse_lazy('login')
     success_message = _('User is successfully registered')
+
+
+class UserUpdateView(UserLoginMixin, UserPermitModifyMixin, SuccessMessageMixin, UpdateView):
+
+    template_name = 'users/update.html'
+    model = User
+    form_class = UserCreateForm
+    success_url = reverse_lazy('users')
+    success_message = _('User is successfully updated')
+    permission_message = _('You have no rights to change another user.')
+    permission_url = reverse_lazy('users')
